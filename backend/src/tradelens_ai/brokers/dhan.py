8f2f01d39828fc98892a14164f29d5f4c05b4a2c
Chaitan Divagi <natiahc@gmail.com>
2026-04-20 18:14:12 +0530

    feat: add Dhan broker adapter skeleton

---
+++ backend/src/tradelens_ai/brokers/dhan.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from tradelens_ai.brokers.base import BrokerAdapter
+from tradelens_ai.domain.models import FundsSnapshot, Holding, Order, OrderRequest, Position
+
+
+class DhanBrokerAdapter(BrokerAdapter):
+    def __init__(self, client_id: str, access_token: str) -> None:
+        self.client_id = client_id
+        self.access_token = access_token
+
+    def broker_name(self) -> str:
+        return "dhan"
+
+    def place_order(self, request: OrderRequest) -> Order:
+        raise NotImplementedError("Dhan adapter not implemented yet")
+
+    def get_order(self, order_id: str) -> Order:
+        raise NotImplementedError("Dhan adapter not implemented yet")
+
+    def list_orders(self) -> list[Order]:
+        raise NotImplementedError("Dhan adapter not implemented yet")
+
+    def cancel_order(self, order_id: str) -> bool:
+        raise NotImplementedError("Dhan adapter not implemented yet")
+
+    def list_positions(self) -> list[Position]:
+        raise NotImplementedError("Dhan adapter not implemented yet")
+
+    def list_holdings(self) -> list[Holding]:
+        raise NotImplementedError("Dhan adapter not implemented yet")
+
+    def get_funds(self) -> FundsSnapshot:
+        raise NotImplementedError("Dhan adapter not implemented yet")
